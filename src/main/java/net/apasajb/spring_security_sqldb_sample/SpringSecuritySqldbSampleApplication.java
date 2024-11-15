0da55ed5c3343047ffa1776a992a95e7e7eda098
ApasaJB <jb.apasa@gmail.com>
2024-11-15 16:48:56 +0100

    init: create the app

---
+++ src/main/java/net/apasajb/spring_security_sqldb_sample/SpringSecuritySqldbSampleApplication.java
@@ -0,0 +1,13 @@
+package net.apasajb.spring_security_sqldb_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySqldbSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySqldbSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/apasajb/spring_security_sqldb_sample/SpringSecuritySqldbSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.apasajb.spring_security_sqldb_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySqldbSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
